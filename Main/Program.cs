590b776a7e317b1dc0798a0b37703716fae8e3e6
Мухоед Александр < @DESKTOP-8459LKN>
2023-03-05 21:43:48 +0700

    Добавьте файлы проекта.

---
+++ Main/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace Main
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = args[0];
+            FileStream file = new FileStream(path, FileMode.Open);
+            StreamReader reader = new StreamReader(file);
+            string line = reader.ReadToEnd();
+            reader.Close();
+            Console.WriteLine(line);
+            Console.WriteLine("Нажмите любую клавишу, а затем Enter");
+            int key1 = Console.Read();
+
+        }
+
+    }
+}
